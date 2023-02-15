23422380c72dd521c89ec6bd3926fe144c0b2ea8
KDH <ysm691611@naver.com>
2023-02-15 16:25:51 +0900

    시간

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -2,17 +2,25 @@ import { useState, useEffect } from "react";
 
 function Clock() {
   const [time, setTime] = useState(new Date());
-
+  const [day,setDay] = useState(new Date())
+  
+  const week = ["일","월", "화", "수","목","금","토"]
   useEffect(() => {
     const id = setInterval(() => {
       setTime(new Date());
+    for(let i=0; i<week.length; i++){
+      setDay(week[day.getDay(i)])
+      console.log([week.getDay(i)])
+    }
+    
     }, 1000);
     return () => clearInterval(id);
   }, []);
-
+  
+  
   return (
     <>
-      <span>{time.toLocaleDateString()}</span><br></br>
+      <span>{time.toLocaleDateString()+day}</span><br></br>
       <span>{time.toLocaleTimeString()}</span>
     </>
   );
